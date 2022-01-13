742588c0c3bc4c72c0d0f7f425647a3f023631bf
Fernando Mireles <fernandodilland@gmail.com>
2022-01-12 19:31:04 -0600

    Update Tutorial_Introducción_Bitso_API.py

--- Introduction to Bitso API/Tutorial_Introducción_Bitso_API.py
+++ Introduction to Bitso API/Tutorial_Introducción_Bitso_API.py
@@ -3,10 +3,10 @@ api = bitso.Api('API_KEY', 'API_SECRET')
 
 # Libros de pedidos disponibles en Bitso
 libros = api.available_books()
-print("Libros disponibles:", libros)
+print("Libros disponibles", libros)
 
-# Extracción de lista
-print("Lista:", libros.books)
+# Extracción de listado
+print("\nLista:", libros.books)
 
-# Extracción de elemento de lista
-print("Elemento 0:", libros.books[0])
+# Extracción de elemento de listado
+print("\nElemento 0:", libros.books[0])
